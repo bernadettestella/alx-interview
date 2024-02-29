54676dd459d81cceb152e1cdaea92dc35729c805
bernadettestella <bernadettestella27@gmail.com>
2024-03-01 01:02:36 +0300

    Making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,26 +12,32 @@ def make_change(coins, total):
        total (int): The amount that needs to be made.
     Returns:
         int: The minimum number of coins required to make up the total.
-    Raises:
-        ValueError: If no combination can add up to the total or if there is an
-        invalid input type.
     """
-    temp_value = 0
-    coins.sort(reverse=True)
-
-    if total < 0:
+    if total <= 0:
         return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
 
-    for coin in coins:
-        if total % coin <= total:
-            temp_value += total // coin
-            total = total % coin
-
-    return temp_value if total == 0 else -1
-
-
-if __name__ == '__main__':
-
-    print(makeChange([1, 2, 25], 37))
-
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
