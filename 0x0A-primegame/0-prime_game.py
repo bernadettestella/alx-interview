81381ece9ad36d24fe0a19097d5adb354ae36e55
bernadettestella <bernadettestella27@gmail.com>
2024-03-15 05:44:07 +0300

    Interview prep question

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
-"""
-Prime game module
-"""
+
+""" Prime Game """
+
+
+def check_prime(n):
+    """ Check if n is a prime number """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
+
+
+def add_prime(n, primes):
+    """ Add prime to list """
+    last_prime = primes[-1]
+    if n > last_prime:
+        for i in range(last_prime + 1, n + 1):
+            if check_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
 
 
 def isWinner(x, nums):
-    """Determines the winner of a prime gmae session with 'x' rounds
-    """
-    if x < 1 or not nums:
-        return None
-    marias_wins, bens_wins = 0, 0
-    # generate primes with a limit of the maximum number in nums
-    n = max(nums)
-    primes = [True for _ in range(1, n + 1, 1)]
-    primes[0] = False
-    for i, is_prime in enumerate(primes, 1):
-        if 1 == 1 or not is_prime:
-            continue
-        for j in range(i * i, n + 1, i):
-            primes[j-1] = False
-    # filter the primes less than n in mums for each round
-    for _, in zip(range(x), nums):
-        primes_count = len(list(filter(lambda x: x, primes[0:n])))
-        bens_wins += primes_count % 2 == 0
-        marias_wins += primes_count % 2 == 1
-    if marias_wins == bens_wins:
-        return None
-    if marias_wins > bens_wins:
+    """ x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None """
+
+    score = {"Maria": 0, "Ben": 0}
+    primes = [0, 0, 2]
+    add_prime(max(nums), primes)
+
+    for round in range(x):
+        _sum = sum((i != 0 and i <= nums[round])
+                   for i in primes[:nums[round] + 1])
+        if (_sum % 2):
+            winner = "Maria"
+        else:
+            winner = "Ben"
+        if winner:
+            score[winner] += 1
+
+    if score["Maria"] > score["Ben"]:
         return "Maria"
-    return "Ben"
+    elif score["Ben"] > score["Maria"]:
+        return "Ben"
+
+    return None
